a8f26643ba7ac13ac9f9000b405aec81caf9e4f3
legrinu <lehona12@yahoo.de>
2017-05-08 19:09:09 +0200

    Update ChangePwCMD.java

--- src/de/legrinu/usermanager/cmd/ChangePwCMD.java
+++ src/de/legrinu/usermanager/cmd/ChangePwCMD.java
@@ -1,48 +1,80 @@
 package de.legrinu.usermanager.cmd;
 
+import de.legrinu.usermanager.utils.Encryption;
+import de.legrinu.usermanager.utils.Manager;
+import java.security.NoSuchAlgorithmException;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import de.legrinu.usermanager.utils.Manager;
-
-public class ChangePwCMD implements CommandExecutor {
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
-		
-		if(sender instanceof Player){
-			Player p = (Player) sender;
-			
-				if(cmd.getName().equals("changepw")){
-					if(args.length == 3){
-						
-					String oldpw = args[0];
-					String pw1 = args[1];
-					String pw2 = args[2];
-					
-					if(Manager.isLoggedIn(p)){
-					if(oldpw.equals(Manager.getPW(p))){
-						if(pw1.equals(pw2)){
-							if(!oldpw.equals(pw1)){
-							Manager.setPW(pw1, p);
-							p.sendMessage("§5[UM]§7 Dein Passwort wurde erfolgreich geändert!");
-							p.sendMessage("§5[UM]§7 Rejoine, damit die Änderungen wirken.");
-							}
-						}
-					}else{
-						p.sendMessage("§5[UM]§7 Falsches Passwort!");
-					}						
-					}
-				}else{
-					sender.sendMessage("§5[UM]§7 /changepw <Altes Passwort> <Neues Passwort> <Neues Passwort>");
-				}
-			}
-		}else{
-	sender.sendMessage("Du musst ein Spieler sein, um dies machen zu dürfen.");
-}
-		return true;
-	}
-
+public class ChangePwCMD
+  implements CommandExecutor
+{
+  public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args)
+  {
+    if ((sender instanceof Player))
+    {
+      Player p = (Player)sender;
+      if (cmd.getName().equals("changepw")) {
+        if (args.length == 3)
+        {
+          String oldpw = args[0];
+          String pw1 = args[1];
+          String pw2 = args[2];
+          if (Manager.isLoggedIn(p))
+          {
+            String oldcrypt = null;
+            try
+            {
+              oldcrypt = Encryption.encrypt(oldpw);
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+              e.printStackTrace();
+            }
+            String crypt = null;
+            try
+            {
+              crypt = Encryption.encrypt(pw1);
+            }
+            catch (NoSuchAlgorithmException e)
+            {
+              e.printStackTrace();
+            }
+            if ((crypt != null) && (oldcrypt != null))
+            {
+              if (oldcrypt.equals(Manager.getPW(p)))
+              {
+                if ((pw1.equals(pw2)) && 
+                  (!oldpw.equals(pw1)))
+                {
+                  Manager.setPW(crypt, p);
+                  p.sendMessage("Â§5[UM]Â§7 Dein Passwort wurde erfolgreich ge#ndert!");
+                  p.sendMessage("Â§5[UM]Â§7 Rejoine, damit die Ã„nderungen wirken.");
+                }
+              }
+              else {
+                p.sendMessage("Â§5[UM]Â§7 Falsches Passwort!");
+              }
+            }
+            else
+            {
+              System.err.println("Error while encrypting! OldPW: " + oldcrypt + " NewPW: " + crypt);
+              p.sendMessage("Â§5[UM]Â§7 Aufgrund eines Fehlers konntest du nicht registriert werden. Melde dich bitte bei einem Teammitglied.");
+            }
+          }
+        }
+        else
+        {
+          sender.sendMessage("Â§5[UM]Â§7 /changepw <Altes Passwort> <Neues Passwort> <Neues Passwort>");
+        }
+      }
+    }
+    else
+    {
+      sender.sendMessage("Du musst ein Spieler sein, um dies machen zu dÃ¼rfen.");
+    }
+    return true;
+  }
 }
